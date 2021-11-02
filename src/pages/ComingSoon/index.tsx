3a5b7a09f3505188950216e4ba8d1f0854092263
Aline Lima <alinelima713@gmail.com>
2021-11-02 11:38:02 -0300

    tela discover iniciada e barra de abas estilizada

---
+++ src/pages/ComingSoon/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Whapper, Heading } from './styles';
+
+const ComingSoon: React.FC = () => {
+  return (
+    <Whapper>
+      <Heading>Coming soon...</Heading>
+    </Whapper>
+  );
+};
+
+export default ComingSoon;
